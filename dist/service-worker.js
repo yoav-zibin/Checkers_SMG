60e50e634007ca8c493a02206f5362f978f93b04
yoav_zibin <yoav.zibin@gmail.com>
2016-10-03 10:28:35 -0400

    x

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -15,7 +15,7 @@ var urlsToCache = [
   "//yoav-zibin.github.io/Checkers_SMG/dist/imgs/avatar_black_crown.svg",
   "//yoav-zibin.github.io/Checkers_SMG/dist/imgs/board.jpg",
 ];
-var CACHE_NAME = 'cache-v2016-09-18T14:48:36.643Z';
+var CACHE_NAME = 'cache-v2016-10-03T14:28:19.534Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
